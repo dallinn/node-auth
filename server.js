cdf290c72e2756667d64dea5ce096adc39500270
dallinn <dallinnickle@gmail.com>
2017-01-23 09:00:45 -0700

    add sequelize and mysql

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var router = express.Router();
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('nodeAuth','root','');
+
+var User = sequelize.define('user', {
+    name: Sequelize.STRING,
+    username: Sequelize.STRING,
+    email: Sequelize.STRING
+});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function(req,res) {
+app.use('/api', router);
+
+router.get('/', function(req,res) {
     res.json({ message: 'json up and running' });
 });
 
