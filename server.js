e4066ae2f39bdd398e35e9ac30ad6905f291ff7c
dallinn <dallinnickle@gmail.com>
2017-01-25 11:23:48 -0700

    remove bloat from server.js

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var router = express.Router();
-var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//Routes
+// Routes
 var Users = require('./app/routes/users');
 var Auth = require('./app/routes/auth');
 app.use('/api', Users, Auth);
