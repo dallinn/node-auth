992253a34a0258105d51452081e022b93645fc21
dallinn <dallinnickle@gmail.com>
2017-01-26 16:02:02 -0700

    react interfacing with api

--- client/src/components/auth.js
+++ client/src/components/auth.js
@@ -1,8 +1,25 @@
 var React = require('react');
 
 var Auth = React.createClass({
+    buttonClicked: function() {
+        //TODO
+        var users = [];
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/users');
+        xhr.send(null);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    users.push(xhr.responseText);
+                } else {
+                    users = 'Error: ' + xhr.status;
+                }
+            } 
+        };
+        console.log(users);
+    },
     render: function() {
-        return <button>Authenticate</button>
+        return <button onClick={this.buttonClicked}>Authenticate</button>
     }
 });
 
