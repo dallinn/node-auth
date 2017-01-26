64d480c1f48a04940199410028230b8646b72240
dallinn <dallinnickle@gmail.com>
2017-01-26 15:32:28 -0700

    first react component

---
+++ client/src/components/auth.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Auth = React.createClass({
+    render: function() {
+        return <button>Authenticate</button>
+    }
+});
+
+module.exports = Auth;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Auth = require('./auth.js');
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <div>
+        <h1>Hello, world!</h1>
+        <Auth />
+        <p>Wow this is a really good front end were going to hire you</p>
+    </div>,
     document.getElementById('app')
 )
